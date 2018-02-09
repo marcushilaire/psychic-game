2b35d702d1a2b5e568c1163b5d19c0bea9a5c25c
unknown <marcushilaire@gmail.com>
2018-02-08 23:06:37 -0500

    added read me and final functionality with button solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,36 +7,44 @@ var losses = 0;
 var lives = 10;
 
 
-// on functions that will load on any keypress
 
-document.onkeyup = function(event){
-    // variable set to the lower case of a key the use presses
-    var userInput = String.fromCharCode(event.which).toLowerCase();
-
-    // slect a number from the array at random
-    var letterSelector = letters[Math.floor(Math.random() * letters.length)];
-
-
-
-   
-    // condition on winning the round or losing a life
-    if (userInput === letterSelector){
-        wins++;
-        tried=[];
-    }
-    else{
-        lives--;
-        tried.push(userInput)
-    };
-
-    if(lives<1){
-        losses++;
-        lives=10;
-        tried=[];        
-    };
- 
-    document.getElementById("guessed").innerHTML="This round have tried: "+ [tried];
-    document.getElementById("wins").innerHTML="You have won " + wins + " times!";
-    document.getElementById("losses").innerHTML="Losses: " +losses;
+function stats(){
+    document.getElementById("guessed").innerHTML="This round you have tried: "+ [tried];        document.getElementById("wins").innerHTML="You have won " + wins + " times!";
+    document.getElementById("losses").innerHTML="Losses: " + losses;
     document.getElementById("lives").innerHTML="lives remaining: " + lives;
 };
+function reset(){
+    lives=10;
+    tried=[];
+};
+
+function randomLetter(){
+    var letterSelector = letters[Math.floor(Math.random() * letters.length)];
+    reset();
+    stats();
+    
+    console.log(letterSelector);
+
+
+    document.onkeyup = function(event){ 
+        var userInput = String.fromCharCode(event.which).toLowerCase();
+
+
+        if (userInput === letterSelector){
+            wins++;
+            randomLetter();
+        }
+        else{
+            lives--;
+            tried.push(userInput)
+        };
+
+        if(lives<1){
+            losses++;
+            randomLetter();
+                   
+        };
+    stats();
+    };
+};
+    
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Psychic Game
+
+## functionality
+* Click the button to select a random letter and begin the game.The button may be clicked at any time to start a new round
+* At the beginning of every new round your lives are replenished and a new random number is selected
+* Pressing a key is all that is required to enter a guess.
+* If you guess incorrectly you will lose a life. If you  lose all your lives you lose the round and another round begins.
+* If you guess correctly you win the round and another round begins.
