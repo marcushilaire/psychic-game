6bde76c8ba1a30a1552cb46d13f495b2f78fa272
unknown <marcushilaire@gmail.com>
2018-02-08 19:00:22 -0500

    javascript work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,27 @@
 // defining variable
-var wins=0;
+var wins=1;
 var losses=0;
-var lives=10
+var lives=10;
+
 // array of letters testing with 5 for now
 var letters= ["a", "b", "c", "d", "e"];
+// slect a number from the array at random
 var letterSelector = letters[Math.floor(Math.random() * letters.length)];
-console.log(letterSelector);
+console.log(" ramdomly selected: "+ letterSelector);
+// on functions that will load on any keypress
+
+document.onkeyup = function(event){
+    var userInput = String.fromCharCode(event.which).toLowerCase();
+    console.log("keypress: "+ userInput);
+    document.getElementById("wins").innerHTML="You have won " + wins + " times!";
+    document.getElementById("lives").innerHTML="lives remaining " + lives;
+    // condition on winning the round or losing a life
+    if (userInput === letterSelector){
+        wins=wins+1;
+    }
+    else{
+        lives=lives-1;
+        // document.getElementById("losses")
+    }
+
+};
