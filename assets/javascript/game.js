0acf4fac8af59f0c42f9b3683006518069d0a94a
unknown <marcushilaire@gmail.com>
2018-02-08 22:01:37 -0500

    removed console.logs and added the full alphabet  to the array of letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // array of letters testing with 5 for now
-var letters= ["a", "b", "c", "d", "e"];
+var letters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var tried=[];
 // defining variable
 var wins = 0;
@@ -12,10 +12,10 @@ var lives = 10;
 document.onkeyup = function(event){
     // variable set to the lower case of a key the use presses
     var userInput = String.fromCharCode(event.which).toLowerCase();
-    console.log("keypress: "+ userInput);
+
     // slect a number from the array at random
     var letterSelector = letters[Math.floor(Math.random() * letters.length)];
-    console.log(" ramdomly selected: "+ letterSelector);
+
 
 
    
@@ -32,10 +32,9 @@ document.onkeyup = function(event){
     if(lives<1){
         losses++;
         lives=10;
+        tried=[];        
     };
-   
-    console.log(tried);
-    // shows stats
+ 
     document.getElementById("guessed").innerHTML="This round have tried: "+ [tried];
     document.getElementById("wins").innerHTML="You have won " + wins + " times!";
     document.getElementById("losses").innerHTML="Losses: " +losses;
